92d7d892e7e0e601f3a14e7c4b7bcd68290dbcd3
virav15 <47110597+virav15@users.noreply.github.com>
2019-02-18 20:15:26 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,29 @@
+import csv
+import os
+
+file_num = 2
+
+
+#file_to_load = "raw_data/budget_data.csv"
+#file_to_output = "analysis/budget_analysis.txt"
+#emply lists for month and revenue data
+file_path = os.path.join("raw_data","budget_data.csv")
+months = []
+revenue = []
+
+#read csv and parse data into lists
+#revenue list will be list of integers
+with open(file_path) as csvfile:
+    csvread = csv.reader(csvfile)
+    
+    next(csvread, None)
+
+    for row in csvread:
+        months.append(row[0])
+        revenue.append(int(row[1]))
+        #find total months
+total_months = len(months)
+
+print("Total Months")
+print(total_months)
 
