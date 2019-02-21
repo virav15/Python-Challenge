4409291fee60c11ed507cf4dcfdb4f1ddfa087cc
virav15 <47110597+virav15@users.noreply.github.com>
2019-02-20 21:52:45 -0500

    a1

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,7 @@ file_num = 2
 file_path = os.path.join("raw_data","budget_data.csv")
 
 months = []
-revenue = []
+profit_loss = []
 
 
 with open(file_path) as csvfile:
@@ -17,26 +17,26 @@ with open(file_path) as csvfile:
 
     for row in csvread:
         months.append(row[0])
-        revenue.append(int(row[1]))
+        profit_loss.append(int(row[1]))
         
 total_months = len(months)
 
 greatest_increase = revenue[0]
 greatest_decrease = revenue[0]
-total_revenue = 0
+total_profit_loss = 0
 
 
-for r in range(len(revenue)):
-    if revenue[r] >= greatest_increase:
-        greatest_increase = revenue[r]
+for r in range(len(profit_loss)):
+    if profit_loss[r] >= greatest_increase:
+        greatest_increase = profit_loss[r]
         great_inc_month = months[r]
-    elif revenue[r] <= greatest_decrease:
-        greatest_decrease = revenue[r]
+    elif profit_loss[r] <= greatest_decrease:
+        greatest_decrease = profit_loss[r]
         great_dec_month = months[r]
-    total_revenue += revenue[r]
+    total_profit_loss += profit_loss[r]
 
 
-average_change = round(total_revenue/total_months, 2)
+average_change = round(total_profit_loss/total_months, 2)
 
 
 output_dest = os.path.join('output_csv','pybank_output' + str(file_num) + '.txt')
@@ -46,7 +46,7 @@ with open(output_dest, 'w') as writefile:
     writefile.writelines('Financial Analysis\n')
     writefile.writelines('----------------------------' + '\n')
     writefile.writelines('Total Months: ' + str(total_months) + '\n')
-    writefile.writelines('Total Revenue: $' + str(total_revenue) + '\n')
+    writefile.writelines('Total Profits/Losses: $' + str(total_profit_loss) + '\n')
     writefile.writelines('Average  Change: $' + str(average_change) + '\n')
     writefile.writelines('Greatest Increase in Profits: ' + great_inc_month + ' ($' + str(greatest_increase) + ')'+ '\n')
     writefile.writelines('Greatest Decrease in Loss: ' + great_dec_month + ' ($' + str(greatest_decrease) + ')')
