92a593525e9e025f5555a9b048c81506c05435a3
virav15 <47110597+virav15@users.noreply.github.com>
2019-02-21 01:18:31 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,71 @@
+import csv
 
+csvpath = 'election_data.csv'
+
+with open(csvpath, 'r') as csvfile:
+
+    reader = csv.reader(csvfile, delimiter=',')
+
+    csvheader = next(reader)
+
+    candidate = []
+
+    for row in reader:
+        candidate.append(row[2])
+
+    candidate_count = [[x,candidate.count(x)] for x in set(candidate)]
+    
+    votes = []
+    name = []
+    
+    for row in candidate_count:
+        name.append(row[0])
+        votes.append(row[1])
+
+    candidate_zip = zip(name, votes)
+    candidate_list = list(candidate_zip)
+
+    winner = max(votes)
+
+    for row in candidate_list:
+        if row[1] == winner:
+            winner_name = row[0]       
+            
+total_votes = len(candidate)
+
+correy_total = candidate.count('Correy')
+correy_percent = int(correy_total) / int(total_votes)
+
+o_tooley_total = candidate.count("O'Tooley")
+o_tooley_percent = o_tooley_total / total_votes
+
+li_total = candidate.count('Li')
+li_percent = li_total / total_votes
+
+khan_total = candidate.count('Khan')
+khan_percent = khan_total / total_votes
+
+print(f'Election Results')
+print(f'-------------------------')
+print(f'Total Votes: {total_votes}')
+print(f'-------------------------')
+print(f'Khan: {khan_percent:.3%} ({khan_total})')
+print(f'Correy: {correy_percent:.3%} ({correy_total})')
+print(f'Li: {li_percent:.3%} ({li_total})')
+print(f"O'Tooley: {o_tooley_percent:.3%} ({o_tooley_total})")
+print(f'-------------------------')
+print(f'Winner: {winner_name}')
+print(f'-------------------------')
+########
+with open('PyPoll.txt', 'w') as text_file:
+    print(f'Election Results', file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Total Votes: {total_votes}', file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Khan: {khan_percent:.3%} ({khan_total})', file=text_file)
+    print(f'Correy: {correy_percent:.3%} ({correy_total})', file=text_file)
+    print(f'Li: {li_percent:.3%} ({li_total})', file=text_file)
+    print(f"O'Tooley: {o_tooley_percent:.3%} ({o_tooley_total})", file=text_file)
+    print(f'-------------------------', file=text_file)
+    print(f'Winner: {winner_name}', file=text_file)
+    print(f'-------------------------', file=text_file)
